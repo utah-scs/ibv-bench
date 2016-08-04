9b2a6d007dbfaeb1bf155be5eb14aa2e018d3d06
Ryan Stutsman <stutsman@cs.stanford.edu>
2016-08-04 10:01:08 -0700

    Improve output noise levels.
    Suppressed some benign failure messages. Script now shows stderr of the
    benchmark process in addition to collecting it in a log file.

--- scripts/emulab.py
+++ scripts/emulab.py
@@ -108,11 +108,11 @@ class BenchmarkRunner(object):
     def send_code(self, server):
         logger.info("Sending code to %s", server)
         subprocess.check_call("rsync -ave ssh --exclude 'logs/*' " +
-                              "./ %s:~/ibv-bench/" % server,
+                              "./ %s:~/ibv-bench/ > /dev/null" % server,
                               shell=True, stdout=sys.stdout)
 
     def compile_code(self, server):
-        logger.info("Compiling code to %s", server)
+        logger.info("Compiling code on %s", server)
         ssh(server, '(cd ibv-bench; (make clean; make -j 8) &> ~/ibv-bench/build.log)')
 
     def start_servers(self):
@@ -136,7 +136,7 @@ class BenchmarkRunner(object):
     def check_huge_pages(self, server):
         r = ssh(server, '~/ibv-bench/scripts/check-hugepages', checked=False)
         if r:
-            logger.error("Checking hugepages failed on %s", server)
+            logger.info("Hugepages are not configured on %s", server)
         return r == 0
 
     def enable_huge_pages(self, server):
@@ -164,9 +164,10 @@ class BenchmarkRunner(object):
                     self.enable_huge_pages(host)
                     some_rebooting = True
             if some_rebooting:
-                raise SystemExit(
-                    "Some machines rebooting to enable hugepages; " +
-                    "restart this script when all machines are back online")
+                logger.warning("Some machines rebooting to enable hugepages; " +
+                               "restart this script when all machines are " +
+                               "back online")
+                raise SystemExit()
 
             for host in self.host_names:
                 self.update_limits(host)
@@ -179,10 +180,11 @@ class BenchmarkRunner(object):
 
             time.sleep(5)
 
-            logger.info("Starting client processes ...\n Benchmarks will take a few hours.")
+            logger.info("Starting client processes; " +
+                        "benchmarks will take a few hours.")
             ssh(self.host_names[0],
                 '(cd ibv-bench; ' +
-                './ibv-bench client %s %s > %s-out.log 2> %s-err.log)'
+                './ibv-bench client %s %s 2>&1 > %s-out.log | tee %s-err.log)'
                     % (' '.join(self.node_names[1:]),
                        self.extra_client_args,
                        self.get_name(),
@@ -196,14 +198,23 @@ class BenchmarkRunner(object):
 def main():
     if not os.path.exists(os.path.join('scripts', 'emulab.py')):
         raise Exception('Run this directly from top-level of the project.')
-    parser = argparse.ArgumentParser(description="\n Runner script to run the benchmark on Apt testbed")
+    parser = argparse.ArgumentParser(description="\n Runner script to run " +
+                                                 "the benchmark on Apt testbed")
     requiredNamed = parser.add_argument_group('Required argument(s)')
-    requiredNamed.add_argument("hostname", nargs=1, help=" hostname of node-0 in the format apt**.apt.emulab.net")
-    Optionals = parser.add_argument_group('Optional arguments')
-    Optionals.add_argument("--clients", type=int, default=None, help="Number of clients. Should be <= n-1 for an experiment with n nodes.")
-    Optionals.add_argument("--log-level", default="INFO", help="python logging level")
-    Optionals.add_argument("--cmd", default="run", help="Method to execute. Defaults to run")
-    Optionals.add_argument("--user", default=None, help="Cloudlab username if different from the current user")
+    requiredNamed.add_argument("hostname", nargs=1,
+                               help=" hostname of node-0 in the format " +
+                                    "apt**.apt.emulab.net")
+    optionals = parser.add_argument_group('Optional arguments')
+    optionals.add_argument("--clients", type=int, default=None,
+                           help="Number of clients. Should be <= n-1 for an " +
+                                "experiment with n nodes.")
+    optionals.add_argument("--log-level", default="INFO",
+                            help="python logging level")
+    optionals.add_argument("--cmd", default="run",
+                           help="Method to execute. Defaults to run")
+    optionals.add_argument("--user", default=None,
+                           help="Cloudlab username if different from the " +
+                                "current user")
     args, unknowns = parser.parse_known_args()
 
     subprocess.check_call('git submodule init', shell=True, stdout=sys.stdout)
