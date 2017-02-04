800c4e1c728fccdaaf47288f8db9bd33656464d1
Aniraj Kesavan <aniraj@cs.utah.edu>
2017-02-03 18:24:31 -0700

    thread local prefix for generateRandom constants

--- Common.cc
+++ Common.cc
@@ -1,9 +1,9 @@
 #include "Common.h"
 
-static uint32_t x = 0xf1e86518;
-static uint32_t y = 0x817271fa;
-static uint32_t z = 0xa8268e81;
-static uint32_t w = 0xb8627c85;
+thread_local static uint32_t x = 0xf1e86518;
+thread_local static uint32_t y = 0x817271fa;
+thread_local static uint32_t z = 0xa8268e81;
+thread_local static uint32_t w = 0xb8627c85;
 
 uint32_t generateRandom() {
     uint32_t t = x ^ (x << 11);
